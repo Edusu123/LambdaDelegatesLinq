dc372885c71c6d6910f680375a73be0ba67ff24d
eduardopires <eduardo.c.pires@outlook.com>
2021-10-28 08:31:22 -0300

    Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum; // assinatura das funções é igual
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum) // assinatura das funções é igual
+
+            double result = op(a, b);
+            //double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
