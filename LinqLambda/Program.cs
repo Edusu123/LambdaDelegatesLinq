04327b4b8043972858bd3b66c2cc0367eb8ffc0b
eduardopires <eduardo.c.pires@outlook.com>
2021-11-03 08:43:56 -0300

    Linq com LAMBDA - Parte 2

--- LinqLambda/Program.cs
+++ LinqLambda/Program.cs
@@ -67,6 +67,36 @@ namespace LinqLambda
 
             var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
             Console.WriteLine($"Single or default: {r9}");
+
+            var r10 = products.Max(p => p.Price); // caso não tenha parâmetro, precisa implementar o IComparable
+            Console.WriteLine($"Max price: {r10}");
+
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine($"Min price: {r11}");
+
+            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine($"Category 1 sum price: {r12}");
+
+            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price); // caso a coleção seja vazia, dispara uma exceção
+            Console.WriteLine($"Category 1 average price: {r13}");
+
+            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Category 5 average price: {r14}");
+
+            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y); // equivalente ao reduce do js
+            Console.WriteLine($"Category 1 aggregate sum: {r15}");                         // 0.0 representa o valor padrão caso o conjunto esteja vazio
+
+            Console.WriteLine();
+
+            var r16 = products.GroupBy(p => p.Category);
+            foreach(IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine($"Category {group.Key.Name}: ");
+                foreach(Product p in group){
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
